e86a9d3e119b78975f4201bcef77ccc00870cd81
DarrenGLDad <100239100+DarrenGLDad@users.noreply.github.com>
2022-08-10 00:32:26 -0400

    Update charts.js, Deliverable 2

--- charts.js
+++ charts.js
@@ -98,5 +98,29 @@ function buildCharts(sample) {
 
 
     // Deliverable 2
+          // 1. Create the trace for the bubble chart.
+      var bubbleData = [
+        {
+          x: otu_ids,
+          y: sample_values,
+          text: otu_labels,
+          mode: "markers",
+          marker: {
+            size: sample_values,
+            color: otu_ids,
+            colorscale: "Navy"
+          }
+        }
+      ];
+      // 2. Create the layout for the bubble chart.
+      var bubbleLayout = {
+        title: "Bacteria Cultures Per Sample",
+        margin: { t: 0 },
+        hovermode: "closest",
+        xaxis: { title: "OTU ID" },
+        margin: { t: 30} 
+      };  
+      // 3. Use Plotly to plot the data with the layout.
+      Plotly.newPlot("bubble", bubbleData, bubbleLayout);
   });
 }
